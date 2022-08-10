a43de0676489b10495356e3b1a559eeb0f009037
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-08-10 15:52:12 -0400

    Update code style

--- 2021/Day4/day1.py
+++ 2021/Day4/day1.py
@@ -1,16 +1,15 @@
-with open("input.txt", "r") as file:
-    lines = file.read().split("\n\n")
+def get_input():
+    with open("input.txt", "r") as file:
+        lines = file.read().split("\n\n")
 
-bingo_cards = []
-bingo_nums = []
-winning_cards = []
+    return lines
 
 
-def main():
-    global bingo_cards
-    global bingo_nums
+def create_cards(file_input):
+    bingo_cards = []
+    bingo_nums = []
     index = 0
-    for card in lines:
+    for card in file_input:
         card = card.lstrip()
         if index == 0:
             card = card.split(",")
@@ -27,30 +26,25 @@ def main():
                 card_num.append(int(num))
             card_line.append(card_num)
         bingo_cards.append(card_line)
+    return bingo_cards, bingo_nums
 
+
+def assess_cards(bingo_cards, bingo_nums):
+
+    winning_cards = []
     nums_called = []
+
     for current_num in bingo_nums:
         for card in bingo_cards:
             nums_called.append(current_num)
 
             if row_check(nums_called, card) or column_check(nums_called, card):
+
                 card_sum = check_sum(nums_called, card)
+                bingo_cards.remove(card)
                 winning_cards.append(card_sum * current_num)
 
-    print(winning_cards[0])
-
-
-def row_check(called_list, card):
-    for row in card:
-        count = 0
-
-        for num in row:
-            if num in called_list:
-                count += 1
-        if count == 5:
-
-            return True
-    return False
+    return winning_cards
 
 
 def column_check(called_list, card):
@@ -71,6 +65,27 @@ def check_sum(called_list, winning_card):
             if num not in called_list:
                 card_sum += num
     return card_sum
+
+
+def row_check(called_list, card):
+    for row in card:
+        count = 0
+
+        for num in row:
+            if num in called_list:
+                count += 1
+        if count == 5:
+
+            return True
+    return False
+
+
+def main():
+    file_input = get_input()
+    bingo_cards, bingo_nums = create_cards(file_input)
+    winning_cards = assess_cards(bingo_cards, bingo_nums)
+
+    print(winning_cards[0])
 
 
 if __name__ == "__main__":
