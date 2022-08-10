8bd4a4bd0fcfb0a8ffdbb66f5a86e55924d9fd96
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-08-10 11:44:00 -0400

    Modify code to simplify logic

--- 2021/Day4/day1.py
+++ 2021/Day4/day1.py
@@ -3,12 +3,7 @@ with open("input.txt", "r") as file:
 
 bingo_cards = []
 bingo_nums = []
-winning_called_num_column = 0
-winning_called_num_row = 0
-winning_row = 0
-winning_column = 0
-winning_index = 9999
-winning_num = 5
+winning_cards = []
 
 
 def main():
@@ -29,97 +24,53 @@ def main():
             card_num = []
             line = line.split()
             for num in line:
-                card_num.append([int(num), 0])
+                card_num.append(int(num))
             card_line.append(card_num)
-            bingo_cards.append(card_line)
+        bingo_cards.append(card_line)
 
-    for called in bingo_nums:
-        row_check(called)
-        column_check(called)
-    print()
-    print("Winning Row", winning_row, "Winning Number", winning_called_num_row)
-    total_count = 0
-    for line in bingo_cards[winning_row]:
-        for num in line:
-            if num[1] == 0:
-                total_count += num[0]
-    print(f'Row {total_count = }')
-    final_row = total_count * winning_called_num_row
+    nums_called = []
+    for current_num in bingo_nums:
+        for card in bingo_cards:
+            nums_called.append(current_num)
 
-    total_count = 0
-    print("Winning Column", winning_column, "Winning Number",
-          winning_called_num_column)
-    for line in bingo_cards[winning_column]:
-        for num in line:
-            if num[1] == 0:
-                total_count += num[0]
-    print(f'Column {total_count = }')
-    final_column = total_count * winning_called_num_column
-    print(f'{final_row = }')
-    print(f'{final_column = }')
+            if row_check(nums_called, card) or column_check(nums_called, card):
+                card_sum = check_sum(nums_called, card)
+                winning_cards.append(card_sum * current_num)
+
+    print(winning_cards[0])
 
 
-def row_check(called):
-    global winning_called_num_row
-    global winning_num
-    global winning_row
-    global winning_index
-    for card in bingo_cards:
-        for line in card:
-            if winning_row != 0:
-                break
-            row_count = 0
-            for num in line:
-                if winning_row != 0:
-                    break
-                if num[0] == called:
-                    num[1] = 1
-                if num[1] == 1:
-                    row_count += 1
-                if row_count == winning_num:
-                    print()
-                    print("Winning Row", bingo_cards.index(card),
-                          "Winning Number",
-                          called)
-                    for line in card:
-                        print(line)
-                    winning_called_num_row = called
-                    winning_row = bingo_cards.index(card)
-                    break
-        # print()
+def row_check(called_list, card):
+    for row in card:
+        count = 0
+
+        for num in row:
+            if num in called_list:
+                count += 1
+        if count == 5:
+
+            return True
+    return False
 
 
-def column_check(called):
-    global winning_called_num_column
-    global winning_index
-    global winning_column
-    index_count = [0, 0, 0, 0, 0]
-    for card in bingo_cards:
-        for line in card:
-            if winning_column != 0:
-                break
-            index_ = 0
-            for num in line:
-                if winning_column != 0:
-                    break
-                if num[0] == called:
-                    num[1] = 1
-                if num[1] == 1:
-                    index_count[index_] += 1
-                for index in index_count:
-                    if winning_column != 0:
-                        break
-                    if index == winning_num:
-                        print()
-                        print("Winning Column", bingo_cards.index(card),
-                              "Winning Number",
-                              called)
-                        for line in card:
-                            print(line)
-                        winning_called_num_column = called
-                        winning_column = bingo_cards.index(card)
-                        break
-                index_ += 1
+def column_check(called_list, card):
+    for row in range(0, 5):
+        count = 0
+        for column in range(0, 5):
+            if card[column][row] in called_list:
+                count += 1
+        if count == 5:
+            return True
+    return False
+
+
+def check_sum(called_list, winning_card):
+    card_sum = 0
+    for row in winning_card:
+        for num in row:
+            if num not in called_list:
+                card_sum += num
+    return card_sum
 
 
 if __name__ == "__main__":
