3cb830d6c01eb452c26939263ca3a87dd199ee24
HexedCoder <92167916+HexedCoder@users.noreply.github.com>
2022-08-11 18:43:46 -0400

    Solve puzzle

--- 2021/Day6/day1.py
+++ 2021/Day6/day1.py
@@ -5,14 +5,46 @@ def get_input():
     return line_input
 
 
-def check_segments(line_segments, planes):
-    pass
+def build_array(file_input):
+    fish_list = [0] * 9
+
+    for fish_count in file_input:
+        fish_count = int(fish_count)
+        fish_list[fish_count] += 1
+
+    return fish_list
+
+
+def progress(current_fish):
+    days = 80
+
+    for day in range(0, days):
+        index = 0
+        saved = 0
+        for timer in current_fish:
+            if index == 0:
+                saved = current_fish[0]
+                current_fish[0] = 0
+            else:
+                if index == 6 or index == 8:
+                    current_fish[index - 1] += current_fish[index]
+                    current_fish[index] = saved
+                else:
+                    current_fish[index - 1] += timer
+                    current_fish[index] = 0
+
+            index += 1
+
+    return current_fish
 
 
 def main():
     fish = get_input()
-    for ind in fish:
-        print(ind)
+    current_fish = build_array(fish)
+
+    fish_count = progress(current_fish)
+
+    print("Total fish after 80 days:", sum(fish_count))
 
 
 if __name__ == "__main__":
