e6af66d5fc031c9a2185f6e8cf230954c0fd6196
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-08-18 08:22:29 -0400

    Update

--- 2021/Day5/solution.py
+++ 2021/Day5/solution.py
@@ -17,7 +17,7 @@ def build_plane():
     return planes
 
 
-def check_segments(line_segments, planes):
+def check_segments_part_1(line_segments, planes1):
     for line_segment in line_segments:
         start_x, start_y = line_segment[0].split(",")
         end_x, end_y = line_segment[1].split(",")
@@ -27,20 +27,46 @@ def check_segments(line_segments, planes):
         distance = max(abs(start_x - end_x), abs(start_y - end_y))
         for _ in range(0, distance + 1):
             if start_x == end_x:
-                planes[start_y][start_x] += 1
+                planes1[start_y][start_x] += 1
                 if start_y < end_y:
                     start_y += 1
                 elif start_y > end_y:
                     start_y -= 1
 
             elif start_y == end_y:
-                planes[start_y][start_x] += 1
+                planes1[start_y][start_x] += 1
                 if start_x < end_x:
                     start_x += 1
                 elif start_x > end_x:
                     start_x -= 1
 
-    return planes
+    return planes1
+
+
+def check_segments_part_2(line_segments, planes2):
+    for line_segment in line_segments:
+        start_x, start_y = line_segment[0].split(",")
+        end_x, end_y = line_segment[1].split(",")
+        start_x, start_y, end_x, end_y = int(start_x), int(start_y), int(
+            end_x), int(end_y)
+
+        planes2[start_x][start_y] += 1
+        distance = max(abs(start_x - end_x), abs(start_y - end_y))
+
+        for moves in range(0, distance):
+            if start_x < end_x:
+                start_x += 1
+            elif start_x > end_x:
+                start_x -= 1
+
+            if start_y < end_y:
+                start_y += 1
+            elif start_y > end_y:
+                start_y -= 1
+
+            planes2[start_x][start_y] += 1
+
+    return planes2
 
 
 def danger_spots(planes):
@@ -49,14 +75,19 @@ def danger_spots(planes):
         for num in plane:
             if num >= 2:
                 bad_spots += 1
-    print(bad_spots)
+    return bad_spots
 
 
 def main():
     line_segments = get_input()
-    planes = build_plane()
-    planes = check_segments(line_segments, planes)
-    danger_spots(planes)
+    planes1 = build_plane()
+    planes2 = build_plane()
+    planes1 = check_segments_part_1(line_segments, planes1)
+    planes2 = check_segments_part_2(line_segments, planes2)
+
+    print("Part 1:", danger_spots(planes1))
+    print()
+    print("Part 2:", danger_spots(planes2))
 
 
 if __name__ == "__main__":
