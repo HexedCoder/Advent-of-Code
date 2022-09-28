5241674f335f297e86ebc41ca80fdd2c18158131
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2022-09-28 15:13:14 -0400

    Modify file name and solve part 2

--- 2021/Day12/solution.py
+++ 2021/Day12/solution.py
@@ -13,47 +13,49 @@ def make_dict(file_input):
     global cave_dict
     for line in file_input:
         line = line.replace("\n", "").split("-")
-        if line[0] not in cave_dict:
+        if line[0] not in cave_dict.keys():
             cave_dict[line[0]] = []
-        cave_dict[line[0]].append(line[1])
-        if line[1].isupper():
-            if line[1] not in cave_dict:
-                cave_dict[line[1]] = []
-                if line[0] not in cave_dict[line[1]]:
-                    cave_dict[line[1]].append(line[0])
-
-        if line[1] not in cave_dict:
+        if line[1] not in cave_dict.keys():
             cave_dict[line[1]] = []
+        cave_dict[line[0]].append(line[1])
         cave_dict[line[1]].append(line[0])
 
 
-def dfs(current_vertex, track):
+def dfs(current_vertex, track, small_caves, part_2):
     global cave_dict
     global lap_count
-
     track = f"{track} {current_vertex}"
 
     if current_vertex != "end":
         for vertex in cave_dict[current_vertex]:
-
             if vertex in cave_dict.keys():
                 if vertex != "start":
-                    if vertex not in track or vertex.isupper():
-                        dfs(vertex, track)
+                    if part_2:
+                        if vertex not in track or vertex.isupper():
+                            dfs(vertex, track, small_caves, 1)
+                        elif small_caves == 0:
+                            dfs(vertex, track, small_caves + 1, 1)
+                    else:
+                        if vertex not in track or vertex.isupper():
+                            dfs(vertex, track, small_caves, 0)
+
     else:
         lap_count += 1
-        print(track)
 
 
 def main():
+    global lap_count
+
     file_input = get_input()
     make_dict(file_input)
 
-    print(cave_dict)
+    dfs("start", "", 0, False)
+    print("Part One:", lap_count)
 
-    dfs("start", "")
+    lap_count = 0
 
-    print(lap_count)
+    dfs("start", "", 0, 1)
+    print("Part Two", lap_count)
 
 
 if __name__ == "__main__":
