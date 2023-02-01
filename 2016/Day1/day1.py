dd2b79f67b39028f2e24034cf5f6144c5b7e6856
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2023-02-01 15:36:00 -0500

    Update solution

--- 2016/Day1/day1.py
+++ 2016/Day1/day1.py
@@ -1,36 +1,49 @@
 def get_input():
-    text_file = open("input", "r").read().split()
+    text_file = open('input').read().split()
     return text_file
 
 
 def main():
-    y_x = [0,0]  # NWSE
+    y_x = [0, 0]  # NS WE
     curr_direction = 0
 
     text_file = get_input()
-    for line in text_file:
-        line = line.replace(",",  "")
+    locations = []
+    repeat = []
 
-        if "R" == line[0]:
+    for line in text_file:
+        line = line.replace(',', '')
+
+        if 'R' == line[0]:
             curr_direction += 1
 
-        elif "L" == line[0]:
+        elif 'L' == line[0]:
             curr_direction -= 1
 
         curr_direction %= 4
 
-        if 0 == curr_direction:
-            y_x[0] -= int(line[1:])
-        elif 1 == curr_direction:
-            y_x[1] += int(line[1:])
-        elif 2 == curr_direction:
-            y_x[0] += int(line[1:])
-        elif 3 == curr_direction:
-            y_x[1] -= int(line[1:])
+        for num in range(int(line[1:])):
 
-    ans = abs(y_x[0]) + abs(y_x[1])
+            if 0 == curr_direction:
+                y_x[0] -= 1
 
-    print("Part 1:", ans)
+            elif 1 == curr_direction:
+                y_x[1] += 1
+
+            elif 2 == curr_direction:
+                y_x[0] += 1
+
+            elif 3 == curr_direction:
+                y_x[1] -= 1
+
+            if [y_x[0], y_x[1]] not in locations:
+                locations.append([y_x[0], y_x[1]])
+            else:
+                if not repeat:
+                    repeat = [y_x[0], y_x[1]]
+
+    print('Part 1:', abs(y_x[0]) + abs(y_x[1]))
+    print('Part 2:', abs(repeat[0]) + abs(repeat[1]))
 
 
 if __name__ == "__main__":
