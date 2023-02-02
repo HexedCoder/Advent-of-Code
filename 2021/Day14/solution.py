71065fb7d317dc112bf0998af7c52cfbe2bc162d
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2023-02-02 12:06:29 -0500

    Simplify solution

--- 2021/Day14/solution.py
+++ 2021/Day14/solution.py
@@ -1,33 +1,33 @@
 def get_input():
-    with open("input") as file_input:
-        lines_input = [line.replace('\n', '') for line in
-                       file_input.read().split('\n') if line]
+    with open("input") as input:
+        lines_input = [line for line in (l.strip() for l in input) if line]
 
     template = lines_input[0]
-    insertion_rules = {}
+    rules = {}
     value_count = {}
 
     for line in lines_input:
         if "->" in line:
             k, v = (line.replace("\n", "").split(" -> "))
 
-            if k not in insertion_rules:
-                insertion_rules[k] = []
+            if k not in rules:
+                rules[k] = []
 
             strings = [f"{k[0]}{v}", f"{v}{k[1]}", f"{v}"]
 
             for string in strings:
-                insertion_rules[k].append(string)
+                rules[k].append(string)
 
-    for key in insertion_rules.keys():
+    for key in rules.keys():
         value_count[key] = 0
 
-    return template, insertion_rules, value_count
+    return template, rules, value_count
 
 
-def initialize_letters(insertion_rules, template):
+def init_letters(rules, template):
     letters = {}
-    for key in insertion_rules.keys():
+
+    for key in rules.keys():
         letters[key[0]] = 0
 
     for letter in template:
@@ -36,19 +36,19 @@ def initialize_letters(insertion_rules, template):
     return letters
 
 
-def initialize_insertion_count(value_count, insertion_rules, letters,
-                               template):
+def init_insert_count(value_count, rules, letters, template):
+
     for idx in range(len(template) - 1):
         index = template[idx:idx + 2]
 
-        for value in insertion_rules[index]:
+        for value in rules[index]:
             if len(value) != 1:
                 value_count[value] += 1
             else:
                 letters[value] += 1
 
 
-def step_through(insertion_rules, value_count, letters):
+def step_through(rules, value_count, letters):
     part_1 = 0
 
     for step in range(2, 41):
@@ -59,7 +59,8 @@ def step_through(insertion_rules, value_count, letters):
             count = values[index]
 
             value_count[key] -= count
-            for value in insertion_rules[key]:
+
+            for value in rules[key]:
                 if len(value) == 2:
                     value_count[value] += count
                 else:
@@ -75,19 +76,16 @@ def step_through(insertion_rules, value_count, letters):
     return part_1, part_2
 
 
-def build_template(template, insertion_rules, value_count):
-    letters = initialize_letters(insertion_rules, template)
+def build_template(template, rules, value_count):
+    letters = init_letters(rules, template)
+    init_insert_count(value_count, rules, letters, template)
 
-    initialize_insertion_count(value_count, insertion_rules, letters,
-                               template)
-
-    return step_through(insertion_rules, value_count, letters)
+    return step_through(rules, value_count, letters)
 
 
 def main():
-    template, insertion_rules, value_count = get_input()
-
-    part_1, part_2 = build_template(template, insertion_rules, value_count)
+    template, rules, value_count = get_input()
+    part_1, part_2 = build_template(template, rules, value_count)
 
     print(f"Part 1: {part_1}")
     print(f"Part 2: {part_2}")
