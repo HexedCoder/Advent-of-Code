48cd08d37fde275b4c216e01ddcc72a1cfb2d471
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2023-02-21 06:26:48 -0500

    Add base code

--- 2021/Day16/solution.py
+++ 2021/Day16/solution.py
@@ -1,35 +1,138 @@
 def get_input():
     file_input = open("input").read()
 
-    return file_input
-
-
-def main():
-    input = get_input()
-
-    output = part_one(input)
-    print("Part 1:", output)
-
-
-def part_one(file_input):
-    print(file_input)
-
     string = ''
     for letter in file_input:
         letter = (int(letter, 16))
         string = f'{string}{letter:04b}'
 
+    print(string)
+    return string
+
+
+def main():
+    string = get_input()
+
+    output = part_one(string)
+    print("Part 1:", output)
+
+
+def get_literal(string, sub_length):
+    num = ''
     version = string[:3]
     type_id = string[3:6]
+    string = string[6:]
+    read = 6
 
-    if '100' == type_id:
-        for _ in range(len(string[7:])):
-            print(string[_], end=' ')
-        print()
+    print(version, type_id, sep='', end='')
 
-    print(string, version, type_id)
+    for start in range(1, len(string[::5]) + 1):
+        sub_type = string[0]
+        print(sub_type, string[1:5], sep='', end='')
 
-    return 0
+        if read + start < sub_length:
+            num += string[1:5]
+            read += 5
+            string = string[5:]
+            if '0' == sub_type:
+                break
+            # else:
+            #     print()
+                # print(string)
+
+        else:
+            print(string[:sub_length - read - start], end='')
+    print(' ', end='')
+    print(f'{num = }')
+
+    return read, string, int(num, 2)
+
+
+def get_operator(string):
+    num = ''
+    version = string[:3]
+    type_id = string[3:6]
+    string = string[6:]
+
+    for start in range(1, len(string[::5]) + 1):
+        sub_type = string[0]
+
+        num += string[1:5]
+        string = string[5:]
+        if '0' == sub_type:
+            break
+
+    print(f'{num = }')
+
+    return string, int(num, 2)
+
+
+def get_length(string, len_id):
+    length = 0
+    if len_id == '0':
+        length = string[:15]
+        print(length, end='')
+    elif len_id == '1':
+        length = string[:11]
+        print(length, end='')
+    return int(length, 2), string[11:] if '1' == len_id else string[15:]
+
+
+def process_packet(string):
+    version = string[:3]
+    type_id = string[3:6]
+    len_id = string[6]
+    # print(string)
+    # print(version, type_id, len_id, sep='', end='')
+
+    return version, type_id, len_id, string[7:]
+
+
+def part_one(string):
+
+    sum = 0
+    version, type_id, len_id, string = process_packet(string)
+
+    sub_length = 0
+
+    read = 0
+
+    if '100' != type_id:
+        sub_length, string = get_length(string, len_id)
+
+        if '0' == len_id:
+            while read < sub_length:
+                ret, string, sub_val = get_literal(string, sub_length)
+                print(sub_val)
+                read += ret
+                sum += sub_val
+        else:
+            while read < sub_length:
+                string, sub_val = get_operator(string)
+                print(sub_val)
+
+
+                sum += sub_val
+                read += 1
+
+    else:
+        if '0' == len_id:
+            while read < sub_length:
+                ret, string, sub_val = get_literal(string, sub_length)
+                print(sub_val)
+
+                read += ret
+                sum += sub_val
+        else:
+            print('d')
+            while read < sub_length:
+                string, sub_val = get_operator(string)
+                print(sub_val)
+
+                sum += sub_val
+                read += 1
+
+    return sum
 
 
 if __name__ == "__main__":
