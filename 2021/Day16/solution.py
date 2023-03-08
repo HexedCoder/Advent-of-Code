4048ac48d9c49126847ba348c39d92620408d33b
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2023-03-08 08:08:38 -0500

    Add solution

--- 2021/Day16/solution.py
+++ 2021/Day16/solution.py
@@ -1,138 +1,107 @@
+import math
+
 def get_input():
-    file_input = open("input").read()
+    file_input = open("s_input").read()
 
     string = ''
     for letter in file_input:
         letter = (int(letter, 16))
         string = f'{string}{letter:04b}'
 
-    print(string)
     return string
 
 
 def main():
     string = get_input()
 
-    output = part_one(string)
+    output = part_one(string, 0)[1]
     print("Part 1:", output)
 
+    output = part_two(string, 0)[1]
+    print("Part 2:", output)
 
-def get_literal(string, sub_length):
-    num = ''
-    version = string[:3]
-    type_id = string[3:6]
-    string = string[6:]
-    read = 6
 
-    print(version, type_id, sep='', end='')
+def part_one(string, idx):
+    version = int(string[idx: idx + 3], 2)
+    type = string[idx + 3: idx + 6]
+    idx += 6
 
-    for start in range(1, len(string[::5]) + 1):
-        sub_type = string[0]
-        print(sub_type, string[1:5], sep='', end='')
-
-        if read + start < sub_length:
-            num += string[1:5]
-            read += 5
-            string = string[5:]
-            if '0' == sub_type:
+    if '100' == type:
+        while True:
+            idx += 5
+            if string[idx - 5] == '0':
                 break
-            # else:
-            #     print()
-                # print(string)
-
-        else:
-            print(string[:sub_length - read - start], end='')
-    print(' ', end='')
-    print(f'{num = }')
-
-    return read, string, int(num, 2)
-
-
-def get_operator(string):
-    num = ''
-    version = string[:3]
-    type_id = string[3:6]
-    string = string[6:]
-
-    for start in range(1, len(string[::5]) + 1):
-        sub_type = string[0]
-
-        num += string[1:5]
-        string = string[5:]
-        if '0' == sub_type:
-            break
-
-    print(f'{num = }')
-
-    return string, int(num, 2)
-
-
-def get_length(string, len_id):
-    length = 0
-    if len_id == '0':
-        length = string[:15]
-        print(length, end='')
-    elif len_id == '1':
-        length = string[:11]
-        print(length, end='')
-    return int(length, 2), string[11:] if '1' == len_id else string[15:]
-
-
-def process_packet(string):
-    version = string[:3]
-    type_id = string[3:6]
-    len_id = string[6]
-    # print(string)
-    # print(version, type_id, len_id, sep='', end='')
-
-    return version, type_id, len_id, string[7:]
-
-
-def part_one(string):
-
-    sum = 0
-    version, type_id, len_id, string = process_packet(string)
-
-    sub_length = 0
-
-    read = 0
-
-    if '100' != type_id:
-        sub_length, string = get_length(string, len_id)
-
-        if '0' == len_id:
-            while read < sub_length:
-                ret, string, sub_val = get_literal(string, sub_length)
-                print(sub_val)
-                read += ret
-                sum += sub_val
-        else:
-            while read < sub_length:
-                string, sub_val = get_operator(string)
-                print(sub_val)
-
-
-                sum += sub_val
-                read += 1
 
     else:
-        if '0' == len_id:
-            while read < sub_length:
-                ret, string, sub_val = get_literal(string, sub_length)
-                print(sub_val)
+        if '0' == string[idx]:
+            packet_end = idx + 16 + int(string[idx + 1: idx + 16], 2)
+            idx += 16
+            while idx < packet_end:
+                idx, ret = part_one(string, idx)
+                version += ret
 
-                read += ret
-                sum += sub_val
         else:
-            print('d')
-            while read < sub_length:
-                string, sub_val = get_operator(string)
-                print(sub_val)
+            size = int(string[idx + 1: idx + 12], 2)
+            idx += 12
+            for _ in range(size):
+                idx, ret = part_one(string, idx)
+                version += ret
 
-                sum += sub_val
-                read += 1
+    return idx, version
 
-    return sum
+
+def part_two(string, idx):
+    type = int(string[idx + 3: idx + 6], 2)
+    idx += 6
+
+    if 4 == type:
+        num = 0
+
+        while True:
+            num = 16*num + int(string[idx + 1:idx + 5], 2)
+            idx += 5
+            if string[idx - 5] == '0':
+                break
+        values = num
+
+    else:
+        values = []
+
+        if '0' == string[idx]:
+            packet_end = idx + 16 + int(string[idx + 1: idx + 16], 2)
+            idx += 16
+            while idx < packet_end:
+                idx, ret = part_two(string, idx)
+                values.append(ret)
+
+        else:
+
+            size = int(string[idx + 1: idx + 12], 2)
+            idx += 12
+            for _ in range(size):
+                idx, ret = part_two(string, idx)
+                values.append(ret)
+
+    version = 0
+
+    if type == 0:
+        version = sum(values)
+    elif type == 1:
+        version = math.prod(values)
+    elif type == 2:
+        version = min(values)
+    elif type == 3:
+        version = max(values)
+    elif type == 4:
+        version = values
+    elif type == 5:
+        version = 1 if values[0] > values[1] else 0
+    elif type == 6:
+        version = 1 if values[0] < values[1] else 0
+    else:
+        version = 1 if values[0] == values[1] else 0
+
+    return idx, version
 
 
 if __name__ == "__main__":
