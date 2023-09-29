24c021ce0c5c821f36a90bb281ce17f697a5aa71
Jacob Hitchcox <jacob.d.hitchcox27@account.cybbh.space>
2023-09-29 09:53:35 -0400

    submit solution

--- 2021/Day17/solution.py
+++ 2021/Day17/solution.py
@@ -1,18 +1,37 @@
 from collections import defaultdict
 
+graph = []
 
-data = open("input").read().strip()
-x_range, y_range = data.split(": ")[1].split(", ")
-x_lower, x_upper = x_range.split("=")[1].split("..")
-x_lower, x_upper = int(x_lower), int(x_upper)
-y_lower, y_upper = y_range.split("=")[1].split("..")
-y_lower, y_upper = int(y_lower), int(y_upper)
+def extract_value(string):
+    return map(int, string.split("=")[1].split(".."))
 
 
-def poi(x_velo, y_velo, area):
+def get_input():
+    with open("input") as file:
+        x_range, y_range = file.readline().strip().split(": ")[1].split(", ")
+
+    x_lower, x_upper = extract_value(x_range)
+    y_lower, y_upper = extract_value(y_range)
+
+    return [x_lower, y_lower, x_upper, y_upper]
+
+
+def main():
+    input_data = get_input()
+
+    output = check_impacts(input_data)
+    print("Part 1:", output[0])
+    print("Part 2:", output[1])
+
+
+def check_impact(input_ranges, check_pos):
+    x_lower, y_lower, x_upper, y_upper = input_ranges
+    x_velo, y_velo = check_pos
+
     step = 0
     x, y = (0, 0)
     max_y = 0
+
     while True:
         if step > 0:
             if x_velo > 0:
@@ -22,33 +41,47 @@ def poi(x_velo, y_velo, area):
             else:
                 x_velo = 0
             y_velo -= 1
+
         x += x_velo
         y += y_velo
-        max_y = y if y > max_y else max_y
 
-        if x < -area or x > x_upper:
+        if y > max_y:
+            max_y = y
+        
+
+        if x < -15000 or x > x_upper:
             break
-        if y < y_lower or y > area:
+        if y < y_lower or y > 15000:
             break
         if x_lower <= x <= x_upper and y_lower <= y <= y_upper:
             return max_y
 
         step += 1
 
+    return None
 
-x_velo_min = 0
-pos = 0
-while pos < x_lower:
-    x_velo_min += 1
-    pos += x_velo_min
 
-velos = defaultdict(int)
-for x_velo in range(x_velo_min, x_upper + 1):
-    for y_velo in range(-7500, 7500):
-        hit = poi(x_velo, y_velo, 7500)
-        if hit != None:
-            velos[(x_velo, y_velo)] = hit
+def check_impacts(input_ranges):
+    x_upper = input_ranges[2]
 
-p1 = max(velos, key=velos.get)
-print(f"Part 1: {velos[p1]} @ {p1}")
-print(f"Part 2: {len(velos)}")
+    max_impact = -10000
+    impact_count = 0
+
+    for x_velocity in range(x_upper + 1):
+        for y_velocity in range(-15000, 15000):
+            hit = check_impact(input_ranges, (x_velocity, y_velocity))
+            if hit != None:
+                impact_count += 1
+                if hit > max_impact:
+                    max_impact = hit
+
+    return max_impact, impact_count
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except (SystemExit, KeyboardInterrupt, GeneratorExit, Exception) as err:
+        from traceback import print_exc
+
+        print_exc()
